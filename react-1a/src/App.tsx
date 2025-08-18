b84e86d71fa7660490cdf462117fdbcb21d846f9
HARIS-MENGAL <harismengaltech@gmail.com>
2025-08-18 12:29:29 +0500

    react first pro

---
+++ react-1a/src/App.tsx
@@ -0,0 +1,22 @@
+import Nav from './components/Navbar'
+import Hero from './components/Hero'
+import Features from './components/Features Section'
+import Faq from './components/Faq'
+import News from './components/News Letter'
+import Footer from './components/Footer'
+import './App.css'
+function App() {
+
+  return (
+    <>
+      <Nav />
+      <Hero />
+      <Features />
+      <Faq />
+      <News />
+      <Footer />
+    </>
+  )
+}
+
+export default App
